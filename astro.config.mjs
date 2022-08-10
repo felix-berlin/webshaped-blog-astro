c8f9d632fa6c0d11b16decff929d4b0ddb0e036d
Felix Scholze <fse@spacesquad.de>
2022-08-11 00:01:10 +0200

    feat: add sitemap and cloudflare support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import vue from '@astrojs/vue';
+import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [preact(), vue()],
+	integrations: [preact(), vue(), sitemap()],
 	site: `http://astro.build`,
+	output: 'server',
+  adapter: cloudflare({ mode: "directory" })
 });
