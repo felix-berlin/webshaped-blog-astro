175571ca01befcd3a2fec5265fa92462be3b42a5
Felix Scholze <fse@spacesquad.de>
2022-08-10 22:00:41 +0200

    feat: add astro

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+import preact from '@astrojs/preact';
+import vue from '@astrojs/vue';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [preact(), vue()],
+	site: `http://astro.build`,
+});
---
+++ src/components/Test.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <button>Mein Vue Button</button>
+  </div>
+</template>
+
+<script>
+export default {
+  setup () {
+
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
