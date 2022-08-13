cca8b2ac650e0fa2f3f8b5abd364d2869614c48c
Felix Scholze <felix@webshaped.de>
2022-08-13 16:13:46 +0200

    build: add stylelint config

---
+++ stylelint.config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  overrides: [
+    {
+      files: ['assets/styles/**/*.{s?(a|c)ss,less,stylus}'],
+      customSyntax: 'postcss-scss'
+    }
+  ],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-standard-scss'
+  ],
+  // plugins: ['stylelint-declaration-block-no-ignored-properties'],
+  // add your custom config here
+  // https://stylelint.io/user-guide/configuration
+  rules: {
+    indentation: 'tab',
+    'string-quotes': 'single',
+    'max-line-length': null,
+    'max-empty-lines': [
+      1,
+      {
+        ignore: ['comments']
+      }
+    ],
+    'selector-class-pattern': null,
+    'at-rule-empty-line-before': [
+      'always',
+      {
+        except: [
+          'after-same-name',
+          'blockless-after-blockless',
+          'first-nested'
+        ],
+        ignore: ['after-comment'],
+        ignoreAtRules: ['else']
+      }
+    ],
+    'selector-type-no-unknown': [
+      true,
+      {
+        ignore: ['custom-elements', 'default-namespace']
+      }
+    ],
+    'no-descending-specificity': null,
+    'function-no-unknown': null
+  }
+}
