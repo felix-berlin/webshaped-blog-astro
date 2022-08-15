6f5873dac15fae27a196cae283359b12d055c309
Felix Scholze <felix@webshaped.de>
2022-08-15 22:17:29 +0200

    feat: add comments components

---
+++ src/components/CommentItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="c-comment">
+    <article :id="'Comment' + comment.commentId" class="c-comment__item">
+      <header class="c-comment__header">
+        <img :src="comment.author.node.avatar.url" alt="" :width="comment.author.node.avatar.width" :height="comment.author.node.avatar.height" class="c-comment__image">
+        <h2>{{ comment.author.node.name }}</h2>
+      </header>
+      <main v-html="comment.content" class="c-comment__content"></main>
+      <footer class="c-comment__footer">
+        <Date :date="comment.dateGmt" class="c-comment__date"></Date>
+      </footer>
+    </article>
+    <div v-for="(reply, index) in comment.replies.nodes" :key="index" v-if="comment.replies" class="HIER">
+      <CommentItem :comment="reply" ></CommentItem>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Date from '../components/Date.vue';
+
+const props = defineProps ({
+  comment: {
+    type: Object,
+    default: () => {},
+    required: true,
+  },
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Comments.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h2>Comments</h2>
+    <template v-for="(comment, index) in comments.nodes" :key="comment.id">
+      <CommentItem :comment="comment"></CommentItem>
+    </template>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { reactive , onMounted } from 'vue';
+import CommentItem from '../components/CommentItem.vue';
+
+const props = defineProps ({
+  comments: {
+    type: Object,
+    default: () => {},
+  },
+})
+
+// const flatListToHierarchical = (
+//     data = [],
+//     {idKey='key',parentKey='parentId',childrenKey='children'} = {}
+// ) => {
+//     const tree = [];
+//     const childrenOf = {};
+//     data.forEach((item) => {
+//         const newItem = {...item};
+//         const { [idKey]: id, [parentKey]: parentId = 0 } = newItem;
+//         childrenOf[id] = childrenOf[id] || [];
+//         newItem[childrenKey] = childrenOf[id];
+//         parentId
+//             ? (
+//                 childrenOf[parentId] = childrenOf[parentId] || []
+//             ).push(newItem)
+//             : tree.push(newItem);
+//     });
+//     return tree;
+// };
+
+
+onMounted(() => {
+})
+</script>
+
+<style scoped>
+
+</style>
