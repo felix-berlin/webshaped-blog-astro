0d860261c2312c093e6f9605ed7e92a615e91b15
Felix Scholze <felix@webshaped.de>
2022-08-15 12:22:45 +0200

    feat: add reading time component

---
+++ src/components/ReadingTime.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <BookOpen/>
+    <time>{{ time }}</time>Min
+  </div>
+</template>
+
+<script setup lang="ts">
+import { BookOpen } from 'lucide-vue-next'
+
+const props = defineProps ({
+  time: {
+    type: Number,
+    default: 0,
+  },
+})
+
+</script>
+
+<style scoped>
+
+</style>
