56ee4522ba2cef72451c2a8d24a7151bab3d665b
Felix Scholze <felix@webshaped.de>
2022-08-15 12:24:04 +0200

    chore: add prefetch and icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import preact from '@astrojs/preact';
 import vue from '@astrojs/vue';
 import sitemap from '@astrojs/sitemap';
 import cloudflare from '@astrojs/cloudflare';
+import prefetch from '@astrojs/prefetch';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [preact(), vue(), sitemap()],
+	integrations: [preact(), vue(), sitemap(), prefetch()],
 	site: `http://astro.build`,
 	// output: import.meta.env.PROD ? 'server' : false,
   // adapter: cloudflare({ mode: "directory" })
