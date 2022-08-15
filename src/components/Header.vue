d7f9faaff3745ffca5f38fc779ba2e0366c12e2e
Felix Scholze <felix@webshaped.de>
2022-08-15 12:22:33 +0200

    feat: add header component

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <header class="wrapper">
+    <nav>
+      <a href="/">Home</a>
+      <ul v-for="(item, index) in menuItems.edges">
+        <li :key="index">
+            <a :href=" '/' + item.node.connectedNode.node.slug">
+              {{ item.node.label }}
+            </a>
+        </li>
+      </ul>
+    </nav>
+    <ColorModeToggle></ColorModeToggle>
+  </header>
+</template>
+
+<script setup lang="ts">
+  // import { props } from '@astrojs/vue';
+import ColorModeToggle from "../components/ColorModeToggle.vue";
+
+const props = defineProps ({
+  menuItems: {
+    type: Object,
+    default: () => {},
+  },
+})
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
