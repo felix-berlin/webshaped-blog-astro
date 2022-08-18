78c6b745d599b5ca744834531ae30b5dfca346bc
Felix Scholze <felix@webshaped.de>
2022-08-18 14:39:49 +0200

    feat: add category component and slug page

---
+++ src/components/Categories.vue
@@ -0,0 +1,30 @@
+<template>
+  <div v-for="(category, index) in categories" :key="index" class="c-categories">
+    <p class="c-categories__item">{{ category.node.name }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+interface Props {
+  categories: {
+    edges: [
+      {
+        node: {
+          name: string;
+          parent: object | null;
+          children: {
+            edges: [];
+          }
+        }
+      }
+    ]
+  };
+}
+
+const props = defineProps<Props>()
+</script>
+
+<style scoped>
+
+</style>
