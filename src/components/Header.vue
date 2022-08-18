6f3c7211c10b058076d33a8d44bfe71738517875
Felix Scholze <felix@webshaped.de>
2022-08-18 14:38:45 +0200

    feat: header nav displays submenu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,26 @@
 <template>
   <header class="wrapper">
     <nav>
-      <a href="/">Home</a>
-      <ul v-for="(item, index) in menuItems.edges">
-        <li :key="index">
-            <a :href=" '/' + item.node.connectedNode.node.slug">
-              {{ item.node.label }}
+      <a href="/">
+        <img src="https://cms.webshaped.de/wp-content/uploads/webshaped_logo_2018_rbg_light.svg" alt="Logo">
+      </a>
+      <ul>
+        <li v-for="(item, index) in menuItems.nodes" :class="{'has-child': item.childItems.nodes.length > 0}" :key="index">
+            <a :href="item.path" v-if="item.childItems.nodes.length === 0">
+              {{ item.label }}
             </a>
+            <Menu
+              v-else
+              :distance="6"
+            >
+              <span>{{ item.label }}</span>
+
+              <template #popper>
+                <template v-for="child in item.childItems.nodes">
+                  <a :href="child.path">{{ child.label }}</a>
+                </template>
+              </template>
+            </Menu>
         </li>
       </ul>
     </nav>
@@ -17,9 +31,28 @@
 <script setup lang="ts">
   // import { props } from '@astrojs/vue';
 import ColorModeToggle from "../components/ColorModeToggle.vue";
+import { Menu } from 'floating-vue';
+import 'floating-vue/dist/style.css';
 
 interface Props {
-  menuItems: object;
+  menuItems: {
+    nodes: [
+      {
+        label: string;
+        order: number;
+        path: string;
+        childItems: {
+          nodes: [
+            {
+              label: string;
+              order: number;
+              path: string;
+            }
+          ]
+        }
+      }
+    ]
+  };
 }
 
 const props = defineProps<Props>()
