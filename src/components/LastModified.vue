ad925be481f8834d95c1c1f7aab266f1bdd2f1f1
Felix Scholze <felix@webshaped.de>
2022-08-19 05:23:44 +0200

    feat: add last modified component

---
+++ src/components/LastModified.vue
@@ -0,0 +1,16 @@
+<template>
+  <time>{{ date }}</time>
+</template>
+
+<script setup lang="ts">
+
+export interface LastModifiedProps {
+  date: string;
+}
+
+const date = defineProps<LastModifiedProps>()
+</script>
+
+<style scoped>
+
+</style>
