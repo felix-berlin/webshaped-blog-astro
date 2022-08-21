127ab84b8c16e9f0a5bf8d5db3c02c9296e0759c
Felix Scholze <felix@webshaped.de>
2022-08-21 21:45:56 +0200

    build: add i18n libs for astro and vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,26 @@
 import { defineConfig } from 'astro/config';
+import { resolve, dirname } from 'node:path'
+import { fileURLToPath } from 'url'
 import preact from '@astrojs/preact';
 import vue from '@astrojs/vue';
 import sitemap from '@astrojs/sitemap';
 import cloudflare from '@astrojs/cloudflare';
 import prefetch from '@astrojs/prefetch';
+import astroI18next from "astro-i18next";
+import vueI18n from '@intlify/vite-plugin-vue-i18n';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [preact(), vue(), sitemap(), prefetch()],
+  experimental: {
+    integrations: true,
+  },
+	integrations: [preact(), vue(), sitemap(), prefetch(), astroI18next({
+    baseLanguage: "de",
+    i18next: {
+      debug: true, // convenient during development to check for missing keys
+      supportedLngs: ["de", "en"],
+    },
+  })],
 	site: `http://astro.build`,
 	// output: import.meta.env.PROD ? 'server' : false,
   // adapter: cloudflare({ mode: "directory" })
@@ -15,6 +28,13 @@ export default defineConfig({
     plugins: [
       vue({
         reactivityTransform: true
+      }),
+      vueI18n({
+        // if you want to use Vue I18n Legacy API, you need to set `compositionOnly: false`
+        // compositionOnly: false,
+
+        // you need to set i18n resource including paths !
+        include: resolve(dirname(fileURLToPath(import.meta.url)), './src/locales/**'),
       })
     ]
   }
