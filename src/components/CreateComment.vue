8deb657c535f8f18c8acb5ed07e9661406f85c64
Felix Scholze <felix@webshaped.de>
2022-08-21 21:44:27 +0200

    feat(create comment): add input validation

--- src/components/CreateComment.vue
+++ src/components/CreateComment.vue
@@ -1,34 +1,124 @@
 <template>
   <div>
     <h2>Create Comment</h2>
-    <input
-      v-model="commentForm.author"
-      type="text"
-      placeholder="Author"
+    <form
+      novalidate="true"
+      @submit.prevent="checkForm"
     >
-    <input
-      v-model="commentForm.email"
-      type="email"
-      placeholder="Email"
-    >
-    <textarea
-      v-model="commentForm.comment"
-      type="text"
-    />
-    <button
-      type="button"
-      @click.prevent="create()"
-    >
-      Submit
-    </button>
+      <div
+        class="c-form__item is-vertical"
+        :class="{'has-error': formErrors.email.length}"
+      >
+        <label
+          class="c-label c-form__label"
+          for="user-email"
+        >Deine E-Mailadresse (optional)</label>
+        <div class="c-floating-label">
+          <input
+            id="user-email"
+            v-model="commentForm.email"
+            class="c-input c-form__input c-input--email c-floating-label__input"
+            type="email"
+            name="user-email"
+            placeholder=" "
+          >
+          <Alert
+            v-if="formErrors.email.length"
+            type="danger"
+            class="c-floating-label__label c-floating-label__label--bottom c-alert--small"
+          >
+            {{ formErrors.email }}
+          </Alert>
+        </div>
+      </div>
+
+      <div
+        class="c-form__item is-vertical"
+        :class="{'has-error': formErrors.author.length}"
+      >
+        <label
+          class="c-form__label c-label is-required"
+          for="author"
+        >Author</label>
+        <div class="c-floating-label">
+          <input
+            id="author"
+            v-model="commentForm.author"
+            class="c-input c-form__input c-floating-label__input"
+            type="text"
+            name="author"
+            placeholder=" "
+          >
+          <Alert
+            v-if="formErrors.author.length"
+            type="danger"
+            class="c-floating-label__label c-floating-label__label--bottom c-alert--small"
+          >
+            {{ formErrors.author }}
+          </Alert>
+        </div>
+      </div>
+
+      <div
+        class="c-form__item is-vertical"
+        :class="{'c-textarea--error': formErrors.comment.length}"
+      >
+        <label
+          class="c-label c-form__label"
+          for="comment"
+        >Schreibe einen Kommentar</label>
+        <div class="c-floating-label">
+          <textarea
+            id="comment"
+            v-model="commentForm.comment"
+            class="c-textarea c-floating-label__input"
+            name="comment"
+            rows="4"
+            placeholder=" "
+          />
+          <Alert
+            v-if="formErrors.comment.length"
+            type="danger"
+            class="c-floating-label__label c-floating-label__label--bottom c-alert--small"
+          >
+            {{ formErrors.comment }}
+          </Alert>
+        </div>
+      </div>
+      <button
+        type="submit"
+      >
+        Submit
+      </button>
+    </form>
   </div>
 </template>
 
 <script setup lang="ts">
+// import { useTranslation } from "i18next-vue";
 import { createComment } from '../lib/api'
 import { ref, onMounted, reactive } from 'vue';
 import { useStore } from '@nanostores/vue';
 import { loadingState } from '../../store/store'
+import Alert from '@components/Alert.vue';
+
+// const { i18next, t } = useTranslation();
+
+// const locales = {
+//   en: {
+//     comment_label: 'Hello!',
+//   },
+//   de: {
+//     comment_label: 'Hallo!',
+//   }
+// };
+
+// i18next.init({
+//   lng: 'en',
+//   resources: {
+//     en: { translation: locales.en },
+//   },
+// });
 
 interface Props {
   currentPostId: number;
@@ -50,6 +140,47 @@ const commentForm: CommentForm = reactive({
   email: '',
 })
 
+const formErrors = reactive({
+  comment: '',
+  author: '',
+  email: '',
+})
+
+const checkForm = () => {
+  // for (const error in formErrors) {
+  //   formErrors[error] = ''
+  // }
+  if (commentForm.comment.length <= 1) {
+    formErrors.comment = 'Kommentar ist zu kurz';
+  }
+
+  if (commentForm.author.length <= 1) {
+    formErrors.author = 'Du hast nen sehr kleinen Namen'
+  }
+
+  // If an e-mail address is given, validate it
+  if (commentForm.email.length > 0 && !validEmail(commentForm.email)) {
+    formErrors.email = 'Bitte überprüfe deine E-Mailadresse, das Format scheint nicht korrekt zu sein.';
+  }
+
+  if (Object.values(formErrors).every(v => v.length === 0)) {
+    create()
+  }
+}
+
+/**
+  * Checks if its a real email address
+  *
+  * @param   {String}  email  E-Mail address
+  *
+  * @return  {Boolean}        If check passes return true
+  */
+const validEmail = (email: string): boolean => {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+  return re.test(email)
+}
+
 async function create() {
   return await createComment(props.currentPostId, commentForm.comment, commentForm.author, commentForm.email)
   .then(({res}) => console.log(res))
@@ -64,6 +195,13 @@ onMounted(() => {
 })
 </script>
 
+
+<!-- <i18n lang="yaml">
+  en:
+    comment_label: 'Hello!'
+  de:
+    comment_label: 'Hallo!'
+</i18n> -->
 <style scoped>
 
 </style>
