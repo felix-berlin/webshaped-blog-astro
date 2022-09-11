352cc1abf0dc57a1b271e4f7b2cbd0c6471499a4
Felix Scholze <felix@webshaped.de>
2022-09-11 18:08:28 +0200

    chore: add vistest coverage

--- tests/unit/LastModified.test.ts
+++ tests/unit/LastModified.test.ts
@@ -2,7 +2,7 @@ import { mount } from '@vue/test-utils'
 import { test, expect } from 'vitest'
 import LastModified from '../../src/components/LastModified.vue'
 
-test('Test LastModified', () => {
+test('Viewing the last modified text', () => {
   let wrapper = mount(LastModified, {
     props: {
       date: '2022-08-17T13:45:09'
