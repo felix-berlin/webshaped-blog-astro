bc540dacf5b41d4df0afe760b3f9d5cafc279586
Felix Scholze <felix@webshaped.de>
2022-10-06 20:49:05 +0200

    feat: add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer>
+    <p>My Footer</p>
+    <a
+      href="https://github.com/felix-berlin"
+      rel="me"
+    >https://github.com/felix-berlin</a>
+  </footer>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
