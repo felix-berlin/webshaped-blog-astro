38d941b7547be9637eb49ef715571701f46c1d63
Felix Scholze <felix@webshaped.de>
2022-10-06 21:57:41 +0200

    feat: add webmention component

---
+++ src/components/Webmention.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    My Webmention's
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue';
+
+const getWebmentions = async () => {
+  const response = await fetch(`https://webmention.io/api/mentions.jf2?target=${window.location.href}`)
+  const data = await response.json()
+  console.log(data);
+
+  return data.children
+}
+
+onMounted(() => {
+  getWebmentions();
+});
+</script>
+
+<style scoped>
+
+</style>
