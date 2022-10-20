92cbdc0482f3c62d8b7a57b7cd11ec424fce9af8
Felix Scholze <felix@webshaped.de>
2022-10-21 01:16:54 +0200

    chore: collect all transition in one file

--- src/components/ColorModeToggle.vue
+++ src/components/ColorModeToggle.vue
@@ -49,13 +49,5 @@ onMounted(() => {
 </script>
 
 <style lang="scss">
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.3s ease;
-}
 
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
 </style>
