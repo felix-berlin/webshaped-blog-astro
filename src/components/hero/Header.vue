58201dd75fb2ad017e65a1d6aef43b9267d2f323
Felix Scholze <felix@webshaped.de>
2022-10-22 15:26:47 +0200

    feat: create Main Nav component

---
+++ src/components/hero/Header.vue
@@ -0,0 +1,42 @@
+<template>
+  <header class="o-header wrapper">
+    <MainNav :menu-items="props.menuItems" />
+    <LanguageSelect />
+    <ColorModeToggle />
+  </header>
+</template>
+
+<script setup lang="ts">
+import ColorModeToggle from '@components/ColorModeToggle.vue';
+import LanguageSelect from '@components/LanguageSelect.vue';
+import MainNav from './MainNav.vue';
+
+export interface HeaderProps {
+  menuItems: {
+    nodes: [
+      {
+        label: string;
+        order: number;
+        path: string;
+        childItems: {
+          nodes: [
+            {
+              label: string;
+              order: number;
+              path: string;
+            }
+          ]
+        }
+      }
+    ]
+  };
+}
+
+const props = defineProps<HeaderProps>()
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/hero/MainNav.vue
@@ -0,0 +1,68 @@
+<template>
+  <nav class="c-main-nav">
+    <a href="/">
+      <img
+        src="https://cms.webshaped.de/wp-content/uploads/webshaped_logo_2018_rbg_light.svg"
+        alt="Logo"
+      >
+    </a>
+    <ul>
+      <li
+        v-for="(item, index) in menuItems.nodes"
+        :key="index"
+        :class="{'has-child': item.childItems.nodes.length > 0}"
+      >
+        <a
+          v-if="item.childItems.nodes.length <= 0"
+          :href="item.path"
+        >
+          {{ item.label }}
+        </a>
+        <VMenu
+          v-else
+          :distance="6"
+        >
+          <span>{{ item.label }}</span>
+
+          <template #popper>
+            <template
+              v-for="(child, childIndex) in item.childItems.nodes"
+              :key="childIndex"
+            >
+              <a :href="child.path">{{ child.label }}</a>
+            </template>
+          </template>
+        </VMenu>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script setup lang="ts">
+export interface MainNavProps {
+  menuItems: {
+    nodes: [
+      {
+        label: string,
+        order: number;
+        path: string;
+        childItems: {
+          nodes: [
+            {
+              label: string;
+              order: number;
+              path: string;
+            }
+          ]
+        }
+      }
+    ]
+  };
+}
+
+const props = defineProps<MainNavProps>()
+</script>
+
+<style scoped>
+
+</style>
