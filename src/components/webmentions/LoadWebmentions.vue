45688a385c835f5f51d3625ae78b9ccf8203a186
Felix Scholze <felix@webshaped.de>
2022-10-30 21:03:52 +0100

    feat(webmentios): load component async and show skelton loader

---
+++ src/components/webmentions/LoadWebmentions.vue
@@ -0,0 +1,83 @@
+<template>
+  <Transition
+    name="fade"
+    mode="in-out"
+  >
+    <Suspense>
+      <Webmentions
+        target="https://develop.webshaped-blog-astro.pages.dev/de/matomo-tracking-script-optimal-einbinden/"
+      />
+
+      <template #fallback>
+        <div class="card is-loading">
+          <div class="image" />
+          <div class="content">
+            <h2 />
+            <p />
+          </div>
+        </div>
+      </template>
+    </Suspense>
+  </Transition>
+</template>
+
+<script setup lang="ts">
+import Webmentions from './Webmentions.vue';
+</script>
+
+<style scoped lang="scss">
+.card {
+  margin: 10px;
+  width: 300px;
+  background: #fff;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.05);
+
+  .image {
+    img {
+      max-width: 100%;
+      border-top-left-radius: 5px;
+      border-top-right-radius: 5px;
+    }
+  }
+
+  .content {
+    padding: 20px 30px;
+  }
+}
+
+
+.card.is-loading {
+  .image,
+  h2,
+  p {
+    background: #eee;
+    background: linear-gradient(110deg, #ececec 8%, #f5f5f5 18%, #ececec 33%);
+    border-radius: 5px;
+    background-size: 200% 100%;
+    animation: 1.5s shine linear infinite;
+  }
+
+  .image {
+    height: 200px;
+    border-bottom-left-radius: 0;
+    border-bottom-right-radius: 0;
+  }
+
+  h2 {
+    height: 30px;
+  }
+
+  p {
+    height: 70px;
+  }
+}
+
+
+@keyframes shine {
+  to {
+    background-position-x: -200%;
+  }
+}
+
+</style>
--- src/components/webmentions/Webmentions.vue
+++ src/components/webmentions/Webmentions.vue
@@ -1,37 +1,54 @@
 <template>
-  <div
-    v-for="(mention, index) in state.mentions"
-    :key="index"
-    class="c-webmentions"
-  >
+  <div>
+    <p>{{ test.name }}</p>
     <div
-      class="c-webmentions__item"
+      v-for="(mention, index) in state.mentions"
+      :key="index"
+      class="c-webmentions"
     >
-      <a
-        :href="mention.author.url"
-        target="_blank"
+      <div
+        class="c-webmentions__item"
       >
-        <img
-          :src="mention.author.photo"
-          :alt="mention.author.name"
-          loading="lazy"
-          decoding="async"
+        <a
+          :href="mention.author.url"
+          target="_blank"
         >
-      </a>
-      <div v-text="mention.content.text" />
+          <img
+            :src="mention.author.photo"
+            :alt="mention.author.name"
+            loading="lazy"
+            decoding="async"
+          >
+        </a>
+        <div v-text="mention.content.text" />
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+
 import { onMounted, reactive } from 'vue';
 import { useStore } from '@nanostores/vue';
 import { currentWebmentionsCount } from '@stores/store';
+
 export interface WebmentionsProps {
   target?: string;
   currentUrl?: boolean;
 }
-
+const loadUserData = async () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        name: 'Matt Maribojoc',
+        pic:
+          'https://cdn-images-1.medium.com/fit/c/100/100/2*EcZb9cndrhTF7_d74dv2Fg.png',
+        bio:
+          'I run a VueJS community over at https://learnvue.co, develop web sites, and post whatever I find cool on the Internet.',
+      })
+    }, 5000)
+  })
+}
 const props = withDefaults(defineProps<WebmentionsProps>(), {
   target: '',
   currentUrl: false,
@@ -41,22 +58,30 @@ const state = reactive({
   mentions: [],
 })
 
-const getWebmentions = async (target = props.target) => {
-  if (props.currentUrl) {
-    target = window.location.href;
-  }
+const test = await loadUserData()
+// const response = await fetch(`https://webmention.io/api/mentions.jf2?target=${props.target}`)
+//                 .then((res) => res.json())
+//                 .then((data) => {
+//                   currentWebmentionsCount.set(data.children.length);
+//                   state.mentions = data.children;
+//                 });
 
-  const response = await fetch(`https://webmention.io/api/mentions.jf2?target=${target}`)
-  const data = await response.json()
-  console.log('Webmentions', data);
-  currentWebmentionsCount.set(data.children.length);
-  state.mentions = data.children
-}
+// const getWebmentions = async (target = props.target) => {
+//   if (props.currentUrl) {
+//     target = window.location.href;
+//   }
 
-onMounted(() => {
-  getWebmentions();
-  console.log(useStore(currentWebmentionsCount));
-});
+//   const response = await fetch(`https://webmention.io/api/mentions.jf2?target=${target}`)
+//   const data = await response.json()
+//   console.log('Webmentions', data);
+//   currentWebmentionsCount.set(data.children.length);
+//   state.mentions = data.children;
+// }
+
+// onMounted( async () => {
+//   await getWebmentions();
+//   console.log(useStore(currentWebmentionsCount));
+// });
 </script>
 
 <style scoped>
