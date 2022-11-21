d2ecf59e41f16a92e35274e4db1afc55011d7529
Felix Scholze <felix@webshaped.de>
2022-11-21 19:35:47 +0100

    chore(toc): add current headline indicator + add smooth scroll for anchor links

--- src/components/TableOfContentsClientSide.vue
+++ src/components/TableOfContentsClientSide.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue';
+import { onMounted, onUnmounted, ref } from 'vue';
 
 export interface TableOfContentsClientSideProps {
   id?: string,
@@ -25,6 +25,7 @@ const props = withDefaults(defineProps<TableOfContentsClientSideProps>(), {
   setIndexIdToHeadlines: false,
 })
 
+let observer = ref<IntersectionObserver>();
 
 const createTocClientSide = () => {
   const toc = document.getElementById(props.id) as HTMLDivElement
@@ -41,6 +42,8 @@ const createTocClientSide = () => {
     const li = document.createElement('li') as HTMLLIElement;
     const a = document.createElement('a') as HTMLAnchorElement;
 
+    a.className = 'c-toc__link';
+    // a.classList.add('c-toc__link');
     if (value.tagName === 'H2') {
       a.innerText = value.textContent;
       a.href = `#${value.id}`;
@@ -62,12 +65,51 @@ const createTocClientSide = () => {
   });
 }
 
+observer.value = new IntersectionObserver((entries) => {
+
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      const headline = entry.target as HTMLElement;
+      const toc = document.getElementById(props.id) as HTMLDivElement;
+      const tocLinks = toc.querySelectorAll('a') as NodeListOf<HTMLAnchorElement>;
+
+
+      tocLinks.forEach((link) => {
+        link.classList.remove('c-toc__link--active');
+      });
+      const tocLink = toc.querySelector(`a[href="#${headline.id}"]`) as HTMLAnchorElement;
+      tocLink.classList.add('c-toc__link--active');
+    }
+  });
+}, {
+  threshold: 0.75,
+  rootMargin: '-10% 0px',
+});
+
 onMounted(() => {
   createTocClientSide()
+  if (observer.value) document.querySelectorAll('.c-blog__content h2[id], .c-blog__content h3[id]').forEach(section => observer.value.observe(section));
+
+  // document.addEventListener('click', (event) => {
+  //   const target = event.target as HTMLElement;
+  //   if (target.classList.contains('c-toc__link')) {
+  //     // event.preventDefault();
+  //     const toc = document.getElementById(props.id) as HTMLDivElement;
+  //     const tocLinks = toc.querySelectorAll('a') as NodeListOf<HTMLAnchorElement>;
+  //     tocLinks.forEach((link) => {
+  //       link.classList.remove('c-toc__link--active');
+  //     });
+  //     target.classList.add('c-toc__link--active');
+  //   }
+  // });
+})
+
+onUnmounted(() => {
+  if (observer.value) observer.value.disconnect();
 })
 
 </script>
 
-<style scoped>
-
+<style lang="scss">
+@use '@styles/components/table-of-contents.scss';
 </style>
---
+++ src/lib/smooth-scroll-anchor.ts
@@ -0,0 +1,29 @@
+export default (function () {
+	const allAnchorLinks = document.querySelectorAll('a[href^="#"]');
+
+	allAnchorLinks.forEach(link => {
+
+		link.addEventListener('click', event => {
+			event.preventDefault();
+
+			const id = event.target.getAttribute('href');
+
+			if (id === '#') return;
+
+			document.querySelector(id)?.scrollIntoView({
+				block: 'start',
+				behavior: 'smooth',
+				inline: 'start',
+			});
+
+  // window.scrollTo({
+  //   behavior: 'smooth',
+  //   top:
+  //     document.querySelector(id).getBoundingClientRect().top -
+  //     document.body.getBoundingClientRect().top -
+  //     90,
+  // })
+
+		})
+	})
+})();
