157f9b6bf602e35d7e04185d115eef602e214950
Felix Scholze <felix@webshaped.de>
2022-11-21 19:42:13 +0100

    chore(post card): add dark mode styles + second background

--- src/components/BlogPostPreview.vue
+++ src/components/BlogPostPreview.vue
@@ -2,32 +2,64 @@
   <article
     v-for="(post, index) in posts.nodes"
     :key="index"
-    class="c-blog-card"
+    class="c-post-card"
   >
-    <a :href="'/' + post.language.slug + '/' + post.slug">
-      <h2>{{ post.title }}</h2>
+    <a
+      class="c-post-card__title-link"
+      :href="'/' + post.language.slug + '/' + post.slug"
+    >
+      <h2 class="c-post-card__title">{{ post.title }}</h2>
     </a>
-    <Date
+    <!-- <Date
       :date="post.dateGmt"
-    />
+    /> -->
 
-    <CommentCount
+    <!-- <CommentCount
       :comment-total="post.commentCount"
       is-element="div"
-    />
+    /> -->
 
-    <ReadingTime :time="post.seo.readingTime" />
+    <!-- <ReadingTime :time="post.seo.readingTime" /> -->
 
-    <HasTranslations
+    <!-- <HasTranslations
       v-if="post.translations && post.translations.length"
       :translations="post.translations"
+    /> -->
+
+    <div
+      class="c-post-card__excerpt"
+      v-html="post.excerpt"
     />
 
-    <div v-html="post.excerpt" />
+    <a
+      class="c-post-card__read-more"
+      :href="'/' + post.language.slug + '/' + post.slug"
+    >
+      <span>Read more</span>
+      <ArrowRight
+        :size="22"
+        class="c-post-card__read-more-arrow"
+      />
+    </a>
+    <!-- <div class="background" style="background-image: image-set()">
+      <img
+        v-if="post.featuredImage"
+        :width="post.featuredImage.node.mediaDetails.width"
+        :height="post.featuredImage.node.mediaDetails.height"
+        :alt="post.featuredImage.node.altText"
+        :src="post.featuredImage.node.mediaItemUrl"
+        :srcset="post.featuredImage.node.srcSet"
+        loading="eager"
+        decoding="async"
+        fetchpriority="high"
+        class="c-blog__hero-image"
+      >
+    </div> -->
   </article>
 </template>
 
 <script setup lang="ts">
+import { ArrowRight } from 'lucide-vue-next';
 import Date from '@components/Date.vue'
 import ReadingTime from '@components/ReadingTime.vue'
 import CommentCount from '@components/comments/CommentCount.vue'
@@ -61,5 +93,5 @@ const props = defineProps<BlogPostPreviewProps>()
 </script>
 
 <style lang="scss">
-@use '@styles/components/blog-card';
+@use '@styles/components/post-card';
 </style>
