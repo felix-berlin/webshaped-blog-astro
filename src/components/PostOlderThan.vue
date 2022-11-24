1de225832d501bb754cf560856b16bdcd8dbd222
Felix Scholze <felix@webshaped.de>
2022-11-24 20:09:19 +0100

    feat(post older than): component; displays hint when post is older than x years
    - Date component show also modified date

---
+++ src/components/PostOlderThan.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-if="getYearDiff(date, Date.now()) >= 4" class="c-post-older-than">
+    <p class="c-post-older-than__text">Dieser Post ist bereits {{ getYearDiff(date, Date.now()) }} alt. Möglicherweise ist der Inhalt nicht mehr ganz aktuell.</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+export interface PostOlderThanProps {
+  date: string;
+}
+
+const props = defineProps<PostOlderThanProps>()
+
+const getYearDiff = (date1: string | number, date2: string | number) => {
+  return Math.abs(new Date(date2).getFullYear() - new Date(date1).getFullYear());
+}
+
+</script>
+
+<style scoped>
+
+</style>
