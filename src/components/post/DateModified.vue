8974e6ebc87db15800b8c2b50f23afa40e2e2ad8
Felix Scholze <felix@webshaped.de>
2022-11-28 23:43:42 +0100

    feat(date modified): add component

---
+++ src/components/post/DateModified.vue
@@ -0,0 +1,26 @@
+<template>
+  <Date :date="date" :lang="lang">
+    <template #before>
+      <Calendar :size="18"></Calendar>
+    </template>
+  </Date>
+</template>
+
+<script setup lang="ts">
+import Date from '@components/Date.vue';
+import { Calendar } from 'lucide-vue-next'
+
+export interface DateModifiedProps {
+  date: string;
+  lang?: {
+    locale: string;
+  };
+}
+
+const props = defineProps<DateModifiedProps>()
+
+</script>
+
+<style scoped>
+
+</style>
