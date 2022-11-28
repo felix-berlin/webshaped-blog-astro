052dc59179ca6d2dc6f13c95bf53ceb6afd5f7dd
Felix Scholze <felix@webshaped.de>
2022-11-28 23:43:06 +0100

    chore(reading time): change icon size

--- src/components/ReadingTime.vue
+++ src/components/ReadingTime.vue
@@ -1,9 +1,10 @@
 <template>
-  <div class="c-reading-time">
+  <div class="c-reading-time" title="Lesezeit">
     <BookOpen
       aria-hidden="true"
       focusable="false"
       class="c-reading-time__icon"
+      :size="18"
     />
     <time class="c-reading-time__time">{{ time }}</time>Min
   </div>
