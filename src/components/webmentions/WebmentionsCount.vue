b6cd360332df67b0ab71397a8a66ea4ae4008314
Felix Scholze <felix@webshaped.de>
2022-11-28 23:39:38 +0100

    refactor(webmentions count): dynamic component

--- src/components/webmentions/WebmentionsCount.vue
+++ src/components/webmentions/WebmentionsCount.vue
@@ -1,19 +1,28 @@
 <template>
-  <div class="c-webmentions-count">
+  <component :is="elementIs" class="c-webmentions-count">
     <AtSign
       aria-label="Webmentions Anzahl"
       class="c-webmentions-count__icon"
+      :size="18"
     />
-    <p class="c-webmentions-count__count">
+    <span class="c-webmentions-count__count">
       {{ count }}
-    </p>
-  </div>
+    </span>
+  </component>
 </template>
 
 <script setup lang="ts">
 import { useStore } from '@nanostores/vue';
 import { currentWebmentionsCount } from '@stores/store';
 import { AtSign } from 'lucide-vue-next'
+
+interface WebmentionsCountProps {
+  elementIs?: string;
+}
+
+const props = withDefaults(defineProps<WebmentionsCountProps>(), {
+  elementIs: 'div'
+});
 
 const count = useStore(currentWebmentionsCount)
 
