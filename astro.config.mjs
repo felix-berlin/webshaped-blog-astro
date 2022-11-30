df78065eba467c3cf616c06a365151fc6c5a77e6
Felix Scholze <felix@webshaped.de>
2022-11-30 10:36:28 +0100

    chore(dependencies): remove preact from project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 import path, { resolve, dirname } from 'node:path'
 import { fileURLToPath } from 'url'
-import preact from '@astrojs/preact';
 import vue from '@astrojs/vue';
 import sitemap from '@astrojs/sitemap';
 import cloudflare from '@astrojs/cloudflare';
 import prefetch from '@astrojs/prefetch';
-import astroI18next from 'astro-i18next';
 import vueI18n from '@intlify/vite-plugin-vue-i18n';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -19,7 +17,6 @@ export default defineConfig({
     integrations: true,
   },
 	integrations: [
-    preact(),
     vue({ appEntrypoint: '/src/pages/_app' }),
     sitemap({
       lastmod: new Date(),
@@ -32,20 +29,11 @@ export default defineConfig({
       },
     }),
     prefetch(),
-    // astroI18next()
   ],
 	// output: import.meta.env.PROD ? 'server' : false,
   // adapter: cloudflare({ mode: "directory" })
   vite: {
-    plugins: [
-      vueI18n({
-        // if you want to use Vue I18n Legacy API, you need to set `compositionOnly: false`
-        // compositionOnly: false,
-
-        // you need to set i18n resource including paths !
-        include: resolve(__dirname, './src/locales/**'),
-      })
-    ],
+    plugins: [],
     resolve: {
       alias: {
         '@sass-butler/': `${path.resolve(__dirname, 'node_modules/@felix_berlin/sass-butler/')}/`,
