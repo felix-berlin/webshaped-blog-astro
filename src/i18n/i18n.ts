232197dc84ab6dbc4ad2429f2b389d2bb0bbddad
Felix Scholze <felix@webshaped.de>
2022-12-02 15:26:29 +0100

    feat(i18n): add pluralisation

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -6,34 +6,61 @@ const availableLanguages = {
 	de: 'Deutsch',
 };
 
-const pluralFormFor = (forms: string, count: number, locale: string) => {
+/**
+ * Selects the correct plural based on the language and the plural object supplied.
+ *
+ * @param   {string}  translationString  translation string
+ * @param   {number}  count              number to determine the plural form
+ * @param   {string}  locale             locale key
+ *
+ * @return  {string}                     return the plural string
+ */
+const pluralFormFor = (translationString: string, count: number, locale: string):string => {
   const matchingForm = new Intl.PluralRules(locale).select(count);
-  return forms[matchingForm];
+
+  return translationString[matchingForm];
 }
 
+/**
+ * Looks up the translation string using the language key.
+ * If variables are present, they are searched for and replaced within the string.
+ * If the string should have a plural object, this will be analyzed before swapping the translation variables.
+ *
+ * @param   {string}  locale             locale key
+ * @param   {string}  translationString  translation string
+ * @param   {object}  varsToReplace      object with variables to replace
+ * @param   {number}  plural             plural form in numbers
+ *
+ * @return  {string}                     return the translated string
+ */
 const __ = (locale: string, translationString: string, varsToReplace?: object, plural?: number):string => {
-  const lang = locale.replace('_', '-');
-  const translations: object = {de_DE, en_US};
+  const lang:string = locale.replace('_', '-');
+  const translations:object = {de_DE, en_US};
 
-  // if (typeof plural === 'number' && plural > 1) {
-  //   // return translations[locale][`${key}--plural`][props.time > 1 ? 1 : 0];
-  //   return pluralFormFor(translations[locale][`${key}--plural`], props.time, locale);
-  // }
-  //
-  // console.log(vars);
-  if (varsToReplace) {
+  // If the translation string is not available, return the translation string itself.
+  let translationStr:string = translations[locale as keyof typeof translations][translationString];
 
+  // If the translation string end with "--plural", execute the plural form function and store the result in the translation string.
+  if (translationString.endsWith("--plural") && plural !== undefined) {
+    translationStr = pluralFormFor(translationStr, plural, lang);
+  }
+
+  // If there is a filled object with variables to replace, replace them.
+  if (varsToReplace !== undefined && Object.keys(varsToReplace).length !== 0) {
+
+    /**
+     * Create a regular expression for each key in the object.
+     * For example: { 'count': 'value', name: 'Jim' } will find {count} and { name } in the translation string.
+     */
     const regex = new RegExp(`\\{\\s*(${Object.keys(varsToReplace).join("|")})\\s*\\}`, 'gi');
 
-    return translations[locale as keyof typeof translations][translationString].replace(regex, (matched:string, offset:number, string:string) => {
+    return translationStr.replace(regex, (matched:string, offset:number, string:string) => {
 
         return varsToReplace[offset as keyof typeof varsToReplace];
     });
   }
 
-
-
-  return translations[locale as keyof typeof translations][translationString] || translationString;
+  return translationStr || translationString;
 }
 
 export { __, availableLanguages };
