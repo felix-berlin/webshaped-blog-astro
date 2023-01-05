f9378102a47b62c9d10a16542638a70aa2937b3d
Felix Scholze <felix@webshaped.de>
2023-01-05 16:59:09 +0100

    chore: del swup

--- src/lib/swup.js
+++
@@ -1,9 +0,0 @@
-import Swup from 'swup';
-import SwupA11yPlugin from '@swup/a11y-plugin'
-import SwupFadeTheme from '@swup/fade-theme';
-import SwupHeadPlugin from '@swup/head-plugin';
-import SwupProgressPlugin from '@swup/progress-plugin';
-
-const swup = new Swup({
-  plugins: [new SwupFadeTheme(), new SwupHeadPlugin(), new SwupA11yPlugin(), new SwupProgressPlugin()],
-});
