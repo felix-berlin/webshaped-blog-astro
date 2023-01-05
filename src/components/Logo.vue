85118087b7ffc1abfbffd4bf27a9102525a4c3a3
Felix Scholze <felix@webshaped.de>
2023-01-06 00:47:00 +0100

    feat: add logo img

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,10 +1,18 @@
 <template>
   <a
-      href="/"
-      class="c-logo"
+    href="/"
+    class="c-logo"
+  >
+    <!-- <span class="c-logo__start">Web</span><span class="c-logo__end">Shaped</span> -->
+    <img
+      src="/public/assets/logo/web_shaped_logo_2023_r.svg"
+      alt="Web Shaped Logo"
+      width="225"
+      height="31"
+      loading="eager"
+      fetchpriority="high"
     >
-      <span class="c-logo__start">Web</span><span class="c-logo__end">Shaped</span>
-    </a>
+  </a>
 </template>
 
 <script setup lang="ts">
