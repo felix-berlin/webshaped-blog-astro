9c8b2b19dc58625ec0156502ee3736a7d02e4d03
Felix Scholze <felix@webshaped.de>
2023-01-05 17:17:03 +0100

    chore: add astro critters to inline css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,45 +1,48 @@
 import { defineConfig } from 'astro/config';
-import path, { resolve, dirname } from 'node:path'
-import { fileURLToPath } from 'url'
+import path, { resolve, dirname } from 'node:path';
+import { fileURLToPath } from 'url';
 import vue from '@astrojs/vue';
 import sitemap from '@astrojs/sitemap';
 import cloudflare from '@astrojs/cloudflare';
 import prefetch from '@astrojs/prefetch';
+import critters from "astro-critters";
 
 const __filename = fileURLToPath(import.meta.url);
+
 const __dirname = dirname(__filename);
+
+
 
 // https://astro.build/config
 export default defineConfig({
   site: `https://webshaped.de`,
   experimental: {
-    integrations: true,
+    integrations: true
   },
-	integrations: [
-    vue({ appEntrypoint: '/src/pages/_app' }),
-    sitemap({
-      lastmod: new Date(),
-      i18n: {
-        defaultLocale: 'de',   // All urls that don't contain `de` or `en` after `https://webshaped.de/` will be treated as default locale, i.e. `de`
-        locales: {
-          de: 'de-DE', // The `defaultLocale` value must present in `locales` keys
-          en: 'en-US',
-        },
-      },
-    }),
-    prefetch(),
-  ],
-	// output: import.meta.env.PROD ? 'server' : false,
+  integrations: [vue({
+    appEntrypoint: '/src/pages/_app'
+  }), sitemap({
+    lastmod: new Date(),
+    i18n: {
+      defaultLocale: 'de',
+      // All urls that don't contain `de` or `en` after `https://webshaped.de/` will be treated as default locale, i.e. `de`
+      locales: {
+        de: 'de-DE',
+        // The `defaultLocale` value must present in `locales` keys
+        en: 'en-US'
+      }
+    }
+  }), prefetch(), critters()],
+  // output: import.meta.env.PROD ? 'server' : false,
   // adapter: cloudflare({ mode: "directory" })
   vite: {
     plugins: [],
     resolve: {
       alias: {
         '@sass-butler/': `${path.resolve(__dirname, 'node_modules/@felix_berlin/sass-butler/')}/`,
-        '@styles/': `${path.resolve(__dirname, 'src/styles/')}/`,
+        '@styles/': `${path.resolve(__dirname, 'src/styles/')}/`
       }
-    },
-    // build: {
+    } // build: {
     //   rollupOptions: {
     //     output: {
     //       entryFileNames: '[name].[hash].js',
@@ -48,5 +51,6 @@ export default defineConfig({
     //     },
     //   },
     // },
+
   }
 });
