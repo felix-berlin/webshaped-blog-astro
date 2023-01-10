56c896f5a2247edbcd9c431773183cccf40453d1
Felix Scholze <felix@webshaped.de>
2023-01-10 22:25:44 +0100

    chore(pagination): add styles

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,51 +1,67 @@
 <template>
-  <nav class="c-pagination">
-    <ul>
+  <nav
+    v-if="page.lastPage > 1"
+    class="c-pagination"
+  >
+    <ul class="c-pagination__list u-list-reset">
       <li
-        v-if="1 !== page.currentPage"
-        class="c-pagination__item"
+        :class="['c-pagination__item', { 'is-disabled': 1 === page.currentPage }]"
       >
-        <a
+        <component
+          :is="1 === page.currentPage ? 'span' : 'a'"
           :href="`${path}/${page.start}`"
           :aria-label="__(lang.locale, 'pagination.first')"
+          class="c-pagination__link"
         >
           <ChevronFirst />
-        </a>
+        </component>
       </li>
       <li
-        v-if="page.url.prev"
-        class="c-pagination__item"
+        :class="['c-pagination__item', { 'is-disabled': !page.url.prev }]"
       >
-        <a
+        <component
+          :is="page.url.prev ? 'a' : 'span'"
           :href="page.url.prev"
           :aria-label="__(lang.locale, 'pagination.previous')"
-        ><ChevronLeft /></a>
+          class="c-pagination__link"
+        >
+          <ChevronLeft />
+        </component>
       </li>
 
       <li
-        v-for="index in page.total"
+        v-for="index in page.lastPage"
         :key="index"
         :class="['c-pagination__item', { 'is-current': index === page.currentPage},]"
       >
-        <a :href="`${path}/${index}`">{{ index }}</a>
+        <a
+          :href="`${path}/${index}`"
+          class="c-pagination__link"
+        >{{ index }}</a>
       </li>
       <li
-        v-if="page.url.next"
-        class="c-pagination__item"
+        :class="['c-pagination__item', { 'is-disabled': !page.url.next }]"
       >
-        <a
+        <component
+          :is="page.url.next ? 'a' : 'span'"
           :href="page.url.next"
           :aria-label="__(lang.locale, 'pagination.next')"
-        ><ChevronRight /></a>
+          class="c-pagination__link"
+        >
+          <ChevronRight />
+        </component>
       </li>
       <li
-        v-if="page.total !== page.currentPage"
-        class="c-pagination__item"
+        :class="['c-pagination__item', { 'is-disabled': page.lastPage === page.currentPage }]"
       >
-        <a
+        <component
+          :is="page.lastPage === page.currentPage ? 'span' : 'a'"
           :href="`${path}/${page.lastPage}`"
           :aria-label="__(lang.locale, 'pagination.last')"
-        ><ChevronLast /></a>
+          class="c-pagination__link"
+        >
+          <ChevronLast />
+        </component>
       </li>
     </ul>
   </nav>
@@ -64,6 +80,6 @@ const props = defineProps<{
 }>();
 </script>
 
-<style scoped>
-
+<style lang="scss">
+@use '@styles/components/pagination'
 </style>
