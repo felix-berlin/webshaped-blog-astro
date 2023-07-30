d680383dc02634c31956e7313b3f3c6bcd8d0e65
Felix Scholze <felix@webshaped.de>
2023-07-30 22:22:12 +0200

    test: add ColorModeToggle test

--- tests/unit/ColorModeToggle.test.ts
+++ tests/unit/ColorModeToggle.test.ts
@@ -1,10 +1,24 @@
 import { mount } from "@vue/test-utils";
-import { it, expect, describe } from "vitest";
+import { it, expect, describe, afterEach, beforeAll } from "vitest";
+import {
+  useTestStorageEngine,
+  setTestStorageKey,
+  cleanTestStorage,
+} from "@nanostores/persistent";
 // @ts-ignore: Unresolved import
 import ColorModeToggle from "@components/ColorModeToggle.vue";
+import { isDarkMode } from "@stores/store";
 
 describe("ColorModeToggle", () => {
+  beforeAll(() => {
+    useTestStorageEngine();
+  });
+
+  afterEach(() => {
+    cleanTestStorage();
+  });
   it("toggles the color mode when clicked", async () => {
+    setTestStorageKey("darkMode", "false");
     const wrapper = mount(ColorModeToggle, {
       global: {
         mocks: {
@@ -16,16 +30,16 @@ describe("ColorModeToggle", () => {
     const button = wrapper.find("button");
     await button.trigger("click");
 
-    expect(localStorage.getItem("darkMode")).toBe("true");
-    expect(wrapper.vm.state.isDark).toBe(true);
+    expect(isDarkMode.get()).toEqual(true);
+    expect(wrapper.vm.isDark).toBe(true);
     expect(document.querySelector("html")?.classList.contains("dark")).toBe(
       true,
     );
 
     await button.trigger("click");
 
-    expect(localStorage.getItem("darkMode")).toBe("false");
-    expect(wrapper.vm.state.isDark).toBe(false);
+    expect(isDarkMode.get()).toEqual(false);
+    expect(wrapper.vm.isDark).toBe(false);
     expect(document.querySelector("html")?.classList.contains("dark")).toBe(
       false,
     );
