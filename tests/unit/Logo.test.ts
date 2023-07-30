0cebadfb09e7877a50abaadc784d5bc9902cb64f
Felix Scholze <felix@webshaped.de>
2023-07-30 22:05:34 +0200

    test: add logo test

--- tests/unit/Logo.test.ts
+++ tests/unit/Logo.test.ts
@@ -1,26 +1,40 @@
 import { mount } from "@vue/test-utils";
-import { it, expect, describe, afterEach } from "vitest";
+import { it, expect, describe, afterEach, beforeAll } from "vitest";
+import {
+  useTestStorageEngine,
+  setTestStorageKey,
+  cleanTestStorage,
+} from "@nanostores/persistent";
 // @ts-ignore: Unresolved import
 import Logo from "@components/Logo.vue";
-import { cleanStores, keepMount } from "nanostores";
 import { isDarkMode } from "@stores/store";
 
 describe("Logo", () => {
-  afterEach(() => {
-    cleanStores(isDarkMode);
+  beforeAll(() => {
+    useTestStorageEngine();
   });
-  it("renders the correct image source based on isDarkMode store", () => {
-    keepMount(isDarkMode);
-    isDarkMode.set(true);
 
-    const wrapper = mount(Logo, {});
+  afterEach(() => {
+    cleanTestStorage();
+  });
 
+  it("has the light logo variant in dark mode", () => {
+    setTestStorageKey("darkMode", "true");
+    const wrapper = mount(Logo);
     const img = wrapper.find("img");
+
+    expect(isDarkMode.get()).toEqual(true);
     expect(img.attributes("src")).toBe(
       "/assets/logo/web_shaped_logo_2023_light.svg",
     );
+  });
 
-    isDarkMode.set(false);
+  it("has the dark logo variant in dark mode", () => {
+    setTestStorageKey("darkMode", "false");
+    const wrapper = mount(Logo);
+    const img = wrapper.find("img");
+
+    expect(isDarkMode.get()).toEqual(false);
     expect(img.attributes("src")).toBe(
       "/assets/logo/web_shaped_logo_2023_dark.svg",
     );
