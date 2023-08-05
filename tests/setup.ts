46afce14cd1aadc644248ee36c81de7c7b4fc988
Felix Scholze <felix@webshaped.de>
2023-08-05 22:48:46 +0200

    test(vitest): sub env & mock resize observer

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,5 +1,8 @@
-import { afterAll, afterEach, beforeAll } from "vitest";
+import { afterAll, afterEach, beforeAll, vi } from "vitest";
 import { config } from "@vue/test-utils";
+import { setupServer } from "msw/node";
+import { graphql, rest } from "msw";
+// import { useAutoAnimate } from "@formkit/auto-animate/vue";
 
 import {
   // Directives
@@ -12,6 +15,8 @@ import {
 } from "floating-vue";
 
 beforeAll(() => {
+  vi.stubEnv("PUBLIC_WP_API", "https://cms.webshaped.de/api");
+
   config.global.components = {
     VDropdown,
     Tooltip,
@@ -20,5 +25,49 @@ beforeAll(() => {
   config.global.directives = {
     "close-popper": VClosePopper,
     tooltip: VTooltip,
+    // "auto-animate": useAutoAnimate,
   };
 });
+
+const ResizeObserverMock = vi.fn(() => ({
+  observe: vi.fn(),
+  unobserve: vi.fn(),
+  disconnect: vi.fn(),
+}));
+vi.stubGlobal("ResizeObserver", ResizeObserverMock);
+
+// const posts = [
+//   {
+//     userId: 1,
+//     id: 1,
+//     title: "first post title",
+//     body: "first post body",
+//   },
+//   // ...
+// ];
+
+// export const restHandlers = [
+//   rest.get("https://rest-endpoint.example/path/to/posts", (req, res, ctx) => {
+//     return res(ctx.status(200), ctx.json(posts));
+//   }),
+// ];
+
+// const graphqlHandlers = [
+//   graphql.query(
+//     "https://graphql-endpoint.example/api/v1/posts",
+//     (req, res, ctx) => {
+//       return res(ctx.data(posts));
+//     },
+//   ),
+// ];
+
+// const server = setupServer(...restHandlers, ...graphqlHandlers);
+
+// // Start server before all tests
+// beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
+
+// //  Close server after all tests
+// afterAll(() => server.close());
+
+// // Reset handlers after each test `important for test isolation`
+// afterEach(() => server.resetHandlers());
