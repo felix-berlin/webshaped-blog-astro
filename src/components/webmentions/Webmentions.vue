3c3885fcd4b50de1107c11d7b77a0e1dcc0f44b4
Felix Scholze <felix@webshaped.de>
2023-08-07 04:24:08 +0200

    fix: type errors

--- src/components/webmentions/Webmentions.vue
+++ src/components/webmentions/Webmentions.vue
@@ -53,30 +53,28 @@ export interface WebmentionsProps {
 }
 
 interface State {
-  mentions: [
-    {
-      author: {
-        name: string;
-        photo: string;
-        url: string;
-        type: string;
-      };
-      content: {
-        html: string;
-        text: string;
-      };
-      "mention-of": string;
-      published: string;
-      type: string;
+  mentions: {
+    author: {
+      name: string;
+      photo: string;
       url: string;
-      "wm-id": number;
-      "wm-private": boolean;
-      "wm-property": string;
-      "wm-received": string;
-      "wm-source": string;
-      "wm-target": string;
-    },
-  ];
+      type: string;
+    };
+    content: {
+      html: string;
+      text: string;
+    };
+    "mention-of": string;
+    published: string;
+    type: string;
+    url: string;
+    "wm-id": number;
+    "wm-private": boolean;
+    "wm-property": string;
+    "wm-received": string;
+    "wm-source": string;
+    "wm-target": string;
+  }[];
 }
 
 const props = withDefaults(defineProps<WebmentionsProps>(), {
@@ -88,21 +86,21 @@ const state: State = reactive({
   mentions: [],
 });
 
-/**
- * Fetch all webmentions for the current page
- *
- * @var {[type]}
- */
-const response = await fetch(
-  `https://webmention.io/api/mentions.jf2?target=${props.target}`,
-)
-  .then((res) => res.json())
-  .then(async (data) => {
-    // TODO: Remove or comment out. This is just for testing.
-    await new Promise((resolve) => setTimeout(resolve, 5000));
-    currentWebmentionsCount.set(data.children.length);
-    state.mentions = data.children;
-  });
+onMounted(async () => {
+  /**
+   * Fetch all webmentions for the current page
+   *
+   * @var {[type]}
+   */
+  await fetch(`https://webmention.io/api/mentions.jf2?target=${props.target}`)
+    .then((res) => res.json())
+    .then(async (data) => {
+      // TODO: Remove or comment out. This is just for testing.
+      await new Promise((resolve) => setTimeout(resolve, 5000));
+      currentWebmentionsCount.set(data.children.length);
+      state.mentions = data.children;
+    });
+});
 
 // const getWebmentions = async (target = props.target) => {
 //   if (props.currentUrl) {
