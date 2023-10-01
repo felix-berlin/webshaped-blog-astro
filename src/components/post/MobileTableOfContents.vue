fc1d5b61c25c98e0ba8827c04381ff03ad10a344
Felix Scholze <felix@webshaped.de>
2023-10-01 02:57:14 +0200

    feat(mobile toc): close on click outside & remove animation

--- src/components/post/MobileTableOfContents.vue
+++ src/components/post/MobileTableOfContents.vue
@@ -1,13 +1,13 @@
 <template>
-  <nav class="c-mobile-toc">
+  <nav ref="mobileToc" class="c-mobile-toc">
     <details ref="toggleButton" class="c-mobile-toc__button-wrap">
       <summary class="c-mobile-toc__button">
         <div class="c-mobile-toc__fake-button c-button">
           <span>{{ __(lang?.locale, "mobile_toc.button") }}</span>
           <ChevronRight class="c-mobile-toc__fake-button-icon" />
         </div>
+
         <span
-          v-auto-animate
           class="c-mobile-toc__active-headline"
           v-text="activeHeadlineText"
         />
@@ -31,6 +31,7 @@ import { useStore } from "@nanostores/vue";
 import { currentLanguage } from "@stores/store";
 import { __ } from "@i18n/i18n";
 import type { TableOfContentsProps } from "@components/post/TableOfContents.vue";
+import { onClickOutside } from "@vueuse/core";
 
 interface MobileTableOfContentsProps {
   headings: TableOfContentsProps["headings"];
@@ -41,6 +42,7 @@ const lang = useStore(currentLanguage);
 const props = defineProps<MobileTableOfContentsProps>();
 const toggleButton = ref<HTMLDetailsElement | null>(null);
 const activeHeadlineText = ref("");
+const mobileToc = ref<HTMLElement | null>(null);
 
 const currentHeadline = (event: string) => (activeHeadlineText.value = event);
 
@@ -48,6 +50,10 @@ const closeDropdown = () => {
   if (!toggleButton.value) return;
   toggleButton.value.open = false;
 };
+
+onClickOutside(mobileToc, (): void => {
+  closeDropdown();
+});
 
 onMounted(() => {
   activeHeadlineText.value = props.headings[0].content;
