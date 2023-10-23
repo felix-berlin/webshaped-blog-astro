a8167b631e666177d49aeed5ee8fa94c54196f23
Felix Scholze <felix@webshaped.de>
2023-10-24 00:14:33 +0200

    fix(categories): comma seperation

--- src/components/Categories.vue
+++ src/components/Categories.vue
@@ -1,21 +1,12 @@
 <template>
-  <div
-    v-if="categories && categories.edges[0].node.name !== 'Uncategorized'"
-    class="c-categories"
-  >
+  <div v-if="categories && categories.edges[0].node.name !== 'Uncategorized'" class="c-categories">
     <p class="c-categories__headline">{{ __(lang?.locale, "categories") }}</p>
     <div class="c-categories__item-wrap">
-      <template
-        v-for="(category, index) in categories.edges"
-        :key="category.node.id"
-      >
-        <a
-          :href="`/category/${category?.node?.name?.toLowerCase()}/1`"
-          class="c-categories__link"
-        >
+      <template v-for="(category, index) in categories.edges" :key="category.node.id">
+        <a :href="`/category/${category?.node?.name?.toLowerCase()}/1`" class="c-categories__link">
           {{ category.node.name }}
-          <span v-if="index !== categories.edges.length - 1">,</span>
         </a>
+        <template v-if="index !== categories.edges.length - 1">, </template>
       </template>
     </div>
   </div>
