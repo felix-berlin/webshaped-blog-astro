ef89086442cd4f912b0ddf037d7e43bebdf2a3c6
Felix Scholze <felix@webshaped.de>
2023-10-27 20:22:11 +0200

    feat(post): add new block + shiki custom theme

--- src/components/ContentBlocks.vue
+++ src/components/ContentBlocks.vue
@@ -1,16 +1,14 @@
 <template>
-  <div id="postContent" class="c-blocks">
-    <template v-for="block in blocks" :key="block.originalContent">
-      <!-- <pre>{{ block }}</pre> -->
-      <ParagraphBlock v-if="block.name === 'core/paragraph'" :block="block" />
-      <ListBlock v-if="block.name === 'core/list'" :block="block" />
-      <HeadlineBlock v-if="block.name === 'core/heading'" :block="block" />
-      <CodeBlock v-if="block.name === 'core/code'" :block="block" />
-      <FigureBlock v-if="block.name === 'core/image'" :block="block" />
-      <ButtonBlock v-if="block.name === 'core/buttons'" :block="block" />
-      <div v-if="block.name === 'core/html'" v-html="block.originalContent" />
-    </template>
-  </div>
+  <template v-for="block in blocks" :key="block.originalContent">
+    <!-- <pre>{{ block }}</pre> -->
+    <ParagraphBlock v-if="block.name === 'core/paragraph'" :block="block" />
+    <ListBlock v-if="block.name === 'core/list'" :block="block" />
+    <HeadlineBlock v-if="block.name === 'core/heading'" :block="block" />
+    <CodeBlock v-if="block.name === 'core/code'" :block="block" />
+    <FigureBlock v-if="block.name === 'core/image'" :block="block" />
+    <ButtonBlock v-if="block.name === 'core/buttons'" :block="block" />
+    <div v-if="block.name === 'core/html'" v-html="block.originalContent" />
+  </template>
 </template>
 
 <script setup lang="ts">
