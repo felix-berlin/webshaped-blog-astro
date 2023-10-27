a9ae7b7d2c1d2ee08e4ea24a957f74b20f080dc8
Felix Scholze <felix@webshaped.de>
2023-10-27 20:20:29 +0200

    feat: add details block

---
+++ src/components/content-blocks/DetailsBlock.vue
@@ -0,0 +1,22 @@
+<template>
+  <details :open="blockAttrs.showContent" class="c-block-details">
+    <summary class="c-block-details__summary">{{ blockAttrs.summary }}</summary>
+    <template v-if="block.innerBlocks?.length">
+      <ContentBlocks :blocks="block.innerBlocks" />
+    </template>
+  </details>
+</template>
+
+<script setup lang="ts">
+import { parse } from "@utils/helpers";
+import ContentBlocks from "@components/ContentBlocks.vue";
+import type { CoreDetailsBlock } from "@ts_types/generated/graphql";
+
+export interface FigureBlockProps {
+  block: CoreDetailsBlock;
+}
+
+const props = defineProps<FigureBlockProps>();
+
+const blockAttrs = parse(props.block.attributesJSON);
+</script>
