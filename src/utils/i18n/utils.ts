d36d1745588518ce0646290f7e65af0595c7dbbb
Felix Scholze <felix@webshaped.de>
2025-02-24 23:50:25 +0100

    docs: add docs blocks for i18n utils

--- src/utils/i18n/utils.ts
+++ src/utils/i18n/utils.ts
@@ -1,14 +1,11 @@
-import type { Maybe } from "@ts_types/generated/graphql";
-import { getDelimiter } from "@utils/helpers";
-import de from "../../content/i18n/de-DE.json";
-import en from "../../content/i18n/en-US.json";
 import { localeStrings, defaultLang, showDefaultLang, routes } from "./ui";
 
-const allTranslationsData = {
-  de_DE: de,
-  en_US: en,
-};
-
+/**
+ * Extracts the language code from the given URL's pathname.
+ *
+ * @param url - The URL object from which to extract the language code.
+ * @returns The extracted language code if it exists in `localeStrings`, otherwise returns `defaultLang`.
+ */
 export const getLangFromUrl = (url: URL) => {
   const [, lang] = url.pathname.split("/");
 
@@ -17,9 +14,35 @@ export const getLangFromUrl = (url: URL) => {
   return defaultLang;
 };
 
+/**
+ * Custom hook to use translations based on the provided language.
+ *
+ * @param   {keyof typeof localeStrings}  lang  The language key to use for translations.
+ *
+ * @return  {function}  A function to get the translated string.
+ *
+ * The returned function `t` has the following parameters:
+ *
+ * @param   {keyof (typeof localeStrings)[typeof defaultLang]}  key             The locale key to look up the translation string.
+ * @param   {object}                                            varsToReplace   An optional object with variables to replace in the translation string.
+ * @param   {number}                                            plural          An optional number to determine the plural form.
+ *
+ * @return  {string}  The translated string, with variables replaced and plural form applied if applicable.
+ */
 export const useTranslations = (lang: keyof typeof localeStrings) => {
   const shortLang = lang?.includes("_") ? (lang.split("_")[0] as keyof typeof localeStrings) : lang;
 
+  /**
+   * Looks up the translation string using the language key.
+   * If variables are present, they are searched for and replaced within the string.
+   * If the string should have a plural object, this will be analyzed before swapping the translation variables.
+   *
+   * @param   {string}  key             locale key
+   * @param   {object}  varsToReplace      object with variables to replace
+   * @param   {number}  plural             plural form in numbers
+   *
+   * @return  {string}                     return the translated string
+   */
   return function t(
     key: keyof (typeof localeStrings)[typeof defaultLang],
     varsToReplace?: object,
@@ -47,6 +70,18 @@ export const useTranslations = (lang: keyof typeof localeStrings) => {
   };
 };
 
+/**
+ * Hook to generate a translated path based on the provided language.
+ *
+ * @param lang - The language key to use for translation, which should be a key of the `localeStrings` object.
+ * @returns A function that translates a given path to the specified language.
+ *
+ * The returned function takes the following parameters:
+ * @param path - The original path to be translated.
+ * @param l - The language key to translate the path to. Defaults to the `lang` parameter.
+ * @returns The translated path. If the path does not have a translation, it returns the original path.
+ * If `showDefaultLang` is false and the language is the default language, it returns the translated path without the language prefix.
+ */
 export const useTranslatedPath = (lang: keyof typeof localeStrings) => {
   return function translatePath(path: string, l: string = lang) {
     const pathName = path.replaceAll("/", "");
@@ -58,6 +93,17 @@ export const useTranslatedPath = (lang: keyof typeof localeStrings) => {
   };
 };
 
+/**
+ * Retrieves the route from a given URL.
+ *
+ * @param url - The URL object from which to extract the route.
+ * @returns The route as a string if found, otherwise `undefined`.
+ *
+ * This function extracts the pathname from the provided URL, splits it into parts,
+ * and attempts to determine the route based on the current language and predefined routes.
+ * If the current language matches the default language, it checks if the path exists in the routes.
+ * If the current language is different, it reverses the lookup to find the corresponding key.
+ */
 export const getRouteFromUrl = (url: URL): string | undefined => {
   const pathname = new URL(url).pathname;
   const parts = pathname?.split("/");
@@ -102,53 +148,4 @@ const pluralFormFor = (translationString: string, count: number, locale: string)
   const matchingForm = pluralRules.select(count);
 
   return translationString[matchingForm as keyof typeof translationString] as string;
-};
-
-/**
- * Looks up the translation string using the language key.
- * If variables are present, they are searched for and replaced within the string.
- * If the string should have a plural object, this will be analyzed before swapping the translation variables.
- *
- * @param   {string}  locale             locale key
- * @param   {string}  translationString  translation string
- * @param   {object}  varsToReplace      object with variables to replace
- * @param   {number}  plural             plural form in numbers
- *
- * @return  {string}                     return the translated string
- */
-export const __ = (
-  locale: Maybe<string>,
-  translationString: string,
-  varsToReplace?: object,
-  plural?: number,
-): string => {
-  const langDelimiter = getDelimiter(locale!);
-
-  const lang = langDelimiter === "-" ? locale : locale?.replace(langDelimiter!, "-");
-
-  let translationStr: string = allTranslationsData[locale][translationString];
-
-  if (typeof translationStr === "undefined" && import.meta.env.DEV) {
-    console.warn(`${translationString} is not available in ${locale}`);
-  }
-
-  // If the translation string end with "--plural", execute the plural form function and store the result in the translation string.
-  if (translationString.endsWith("--plural") && plural !== undefined && lang) {
-    translationStr = pluralFormFor(translationStr, plural, lang);
-  }
-
-  // If there is a filled object with variables to replace, replace them.
-  if (varsToReplace !== undefined && Object.keys(varsToReplace).length !== 0) {
-    /**
-     * Create a regular expression for each key in the object.
-     * For example: { 'count': 'value', name: 'Jim' } will find {count} and { name } in the translation string.
-     */
-    const regex = new RegExp(`\\{\\s*(${Object.keys(varsToReplace).join("|")})\\s*\\}`, "gi");
-
-    return translationStr.toString().replace(regex, (matched: string, offset: number) => {
-      return varsToReplace[offset as keyof typeof varsToReplace];
-    });
-  }
-
-  return translationStr || translationString;
 };
