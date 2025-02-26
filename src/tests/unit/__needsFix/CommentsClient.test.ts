679e2e3bed61f4c561d6919a910378637374e218
Felix Scholze <felix@webshaped.de>
2025-02-26 13:07:43 +0100

    ci: test to fix folder

--- src/tests/unit/__needsFix/CommentsClient.test.ts
+++ src/tests/unit/__needsFix/CommentsClient.test.ts
@@ -1,7 +1,6 @@
 import { mount } from "@vue/test-utils";
 import { nextTick } from "vue";
 import { it, expect, describe, beforeAll, vi } from "vitest";
-// @ts-ignore: Unresolved import
 import CommentsClient from "@components/comments/CommentsClient.vue";
 
 describe("CommentsClient", () => {
