5ff835759962df411e1c7822d29530570f0a7ae3
Felix Scholze <felix@webshaped.de>
2025-03-31 22:24:07 +0200

    feat: replace current rss link by component

--- src/components/main-nav/ButtonBar.vue
+++ src/components/main-nav/ButtonBar.vue
@@ -8,13 +8,7 @@
       <!-- FIXME: PWA is not working (manifest is not found ect.) -->
       <!-- <InstallApp :show-icon="true" :show-text="false" :icon-size="24" class="c-button--icon" /> -->
 
-      <a
-        class="c-main-nav__rss-link c-button c-button--icon"
-        href="/rss.xml"
-        :aria-label="t('rss_feed.link_title')"
-      >
-        <Rss class="c-main-nav__rss-icon" />
-      </a>
+      <RssLink class="c-main-nav__rss-link" />
 
       <ColorModeToggle class="c-main-nav__color-toggle" />
     </div>
@@ -22,12 +16,9 @@
 </template>
 
 <script setup lang="ts">
-import Rss from "virtual:icons/lucide/rss";
-import { useI18n } from "@/composables/useI18n";
 // import InstallApp from "@components/InstallApp.vue";
 import ColorModeToggle from "@components/ColorModeToggle.vue";
 import SearchModal from "@components/main-nav/SearchModal.vue";
 import LanguageDropdown from "@components/LanguageDropdown.vue";
-
-const { t } = useI18n();
+import RssLink from "@components/RssLink.vue";
 </script>
