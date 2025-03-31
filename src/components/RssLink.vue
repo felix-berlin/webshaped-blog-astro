549c0392f700eb2abcda4cd176df2b0c9f54e3ff
Felix Scholze <felix@webshaped.de>
2025-03-31 22:23:44 +0200

    feat: create RssLink component

---
+++ src/components/RssLink.vue
@@ -0,0 +1,22 @@
+<template>
+  <a
+    class="c-button c-button--icon"
+    :href="`/${lang}/rss.xml`"
+    target="_blank"
+    :aria-label="t('rss_feed.link_title')"
+  >
+    <slot>
+      <Rss class="c-main-nav__rss-icon" />
+    </slot>
+  </a>
+</template>
+
+<script setup lang="ts">
+import Rss from "virtual:icons/lucide/rss";
+import { useI18n } from "@/composables/useI18n";
+import { currentLanguage } from "@stores/store";
+import { useStore } from "@nanostores/vue";
+
+const lang = useStore(currentLanguage);
+const { t } = useI18n();
+</script>
