0c73e29d1940d7d06ef0c4abae3508f9ef0814f5
Felix Scholze <felix@webshaped.de>
2025-05-03 22:44:44 +0200

    feat: remove unused window types

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,13 +1,5 @@
-import type { TranslationRoutes } from "@layouts/DefaultLayout.astro";
-
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 /// <reference types="../.astro/astro-env.d.ts" />
 declare module "@pagefind/default-ui";
-
-// Extend the Window interface
-declare global {
-  interface Window {
-    translationRoutes?: TranslationRoutes;
-  }
-}
+export {};
