562a6188c414c9072caf13e9c412e70d45419c82
Felix Scholze <felix@webshaped.de>
2025-05-04 20:44:47 +0200

    feat: add urql vue; use new CreateComment mutation

--- src/services/createComment.ts
+++ src/services/createComment.ts
@@ -1,46 +1,35 @@
-import { fetchAPI } from "@services/fetchApi";
-import type { CreateCommentInput } from "@ts_types/generated/graphql";
+import { graphql } from "@/gql";
 
-/**
- * creates a new comment
- *
- * @export
- * @param {CreateCommentInput["commentOn"]} commentOn - id of the post
- * @param {CreateCommentInput["content"]} content - content of the comment
- * @param {CreateCommentInput["author"]} author - name of the author
- * @param {CreateCommentInput["authorEmail"]} [authorEmail] - email of the author
- * @param {CreateCommentInput["parent"]} [parent] - id of the parent comment
- * @return {*}  {Promise}
- */
-export const createComment = async (
-  commentOn: CreateCommentInput["commentOn"],
-  content: CreateCommentInput["content"],
-  author: CreateCommentInput["author"],
-  authorEmail?: CreateCommentInput["authorEmail"],
-  parent?: CreateCommentInput["parent"],
-) => {
-  return await fetchAPI(`
-    mutation {
-      createComment(input: {
-        commentOn: ${commentOn},
-        content: "${content}",
-        author: "${author}",
-        authorEmail: "${authorEmail}",
-        parent: "${parent}"
-      }) {
-        comment {
-          id
-          content
-          status
-          isRestricted
-          author {
-            node {
-              name
-              email
-            }
+export const CreateComment = graphql(`
+  mutation CreateComment(
+    $author: String = ""
+    $authorEmail: String = ""
+    $content: String = ""
+    $parent: ID = ""
+    $commentOn: Int = 1
+  ) {
+    createComment(
+      input: {
+        author: $author
+        authorEmail: $authorEmail
+        commentOn: $commentOn
+        content: $content
+        parent: $parent
+      }
+    ) {
+      comment {
+        id
+        content
+        status
+        isRestricted
+        author {
+          node {
+            name
+            email
           }
         }
-        success
       }
-    }`);
-};
+      success
+    }
+  }
+`);
