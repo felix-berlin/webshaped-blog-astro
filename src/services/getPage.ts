1c4cbba844515ef71a2491efeda197985d817206
Felix Scholze <felix@webshaped.de>
2025-05-04 20:43:23 +0200

    refactor: optimize page requests

--- src/services/getPage.ts
+++ src/services/getPage.ts
@@ -1,59 +1,21 @@
-import type { RootQuery } from "@ts_types/generated/graphql";
-import { fetchAPI } from "@services/fetchApi";
+import { graphql } from "@/gql";
 
-export const getAllPagesWithSlugs = async (): Promise<RootQuery["pages"]> => {
-  const data = await fetchAPI(`
-  {
-    pages(first: 10000) {
-      edges {
-        node {
-          slug
-        }
-      }
-    }
-  }
-  `);
-  return data?.pages;
-};
-
-export const getPageBySlug = async (
-  slug: string,
-): Promise<RootQuery["page"]> => {
-  const data = await fetchAPI(`
-  {
-    page(id: "${slug}", idType: URI) {
-      slug
-      title
-      content
-      language {
-        code
-        locale
-        name
-      }
-      seo {
+export const GetPagesBySlugs = graphql(`
+  query GetPagesBySlugs($slugs: [String] = []) {
+    pages(where: { nameIn: $slugs }) {
+      nodes {
+        slug
         title
-        readingTime
-        canonical
-        metaDesc
-        opengraphSiteName
-        opengraphAuthor
-        opengraphDescription
-        opengraphPublisher
-        opengraphTitle
-        opengraphType
-        opengraphUrl
-        opengraphPublishedTime
-        opengraphModifiedTime
-        opengraphImage {
-          sourceUrl
+        content
+        language {
+          code
+          locale
+          name
         }
-        twitterDescription
-        twitterTitle
-        metaRobotsNofollow
-        metaRobotsNoindex
+        seo {
+          ...PostTypeSeoFragment
+        }
       }
     }
   }
-  `).then((res) => res.data);
-  return data?.page;
-};
+`);
