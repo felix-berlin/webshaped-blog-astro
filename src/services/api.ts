cdb4d36fc3aaf475a75ff955512bf4e59fd7d624
Felix Scholze <felix@webshaped.de>
2025-05-08 14:40:36 +0200

    fix: missing preview post on index

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,3 @@
-import type { CreateCommentPayload } from "@ts_types/generated/graphql";
-
 export * from "@services/mutations/createComment";
 export * from "@services/queries/getMenu";
 export * from "@services/queries/getCategory";
@@ -8,33 +6,3 @@ export * from "@services/queries/getComment";
 export * from "@services/queries/getPost";
 export * from "@services/queries/getPostPreview";
 export * from "@services/queries/getAuthor";
-
-interface Error {
-  message?: string;
-  extensions?: {
-    category: string;
-  };
-  locations?: {
-    line: number;
-    column: number;
-  }[];
-  path?: string[];
-}
-
-interface QueryResult {
-  errors?: Error[];
-  extensions?: {
-    debug: Array<string | object | boolean>;
-    queryAnalyzer: {
-      keys: string;
-      keysLength: number;
-      keysCount: number;
-      skippedKeys: string;
-      skippedKeysSize: number;
-      skippedKeysCount: number;
-      skippedTypes: Array<string | object | boolean>;
-    };
-  };
-}
-
-export interface CreateCommentPayloadExtended extends CreateCommentPayload, QueryResult {}
